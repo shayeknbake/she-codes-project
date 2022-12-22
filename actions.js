2376f5987783ff2f32b8cf6d013dcbed5309aa19
shayeknbake <120424551+shayeknbake@users.noreply.github.com>
2022-12-22 13:59:29 -0800

    added she codes API Key to JS and updated Days of week

--- actions.js
+++ actions.js
@@ -73,7 +73,7 @@ function locationWeather(response) {
 }
 
 function searchLocation(position) {
-  let apiKey = "5ef4de8cd6b7fefcd7c42f98cf464ce8";
+  let apiKey = "932fota4afb850a2171c8773fb965c10";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(locationWeather);
